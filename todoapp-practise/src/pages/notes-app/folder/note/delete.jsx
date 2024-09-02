0a725b70e900ee4e8afd35962223e190e63bfe5d
phamLinh98 <linhthusinh98@gmail.com>
2024-09-02 23:39:47 +0900

    commit code

--- todoapp-practise/src/pages/notes-app/folder/note/delete.jsx
+++ todoapp-practise/src/pages/notes-app/folder/note/delete.jsx
@@ -1,1 +1,9 @@
-//TODO: add delete
+const DeleteNote = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <h1>Delete note</h1>
+    </div>
+  );
+};
+
+export default DeleteNote;
--- todoapp-practise/src/pages/notes-app/folder/note/update.jsx
+++ todoapp-practise/src/pages/notes-app/folder/note/update.jsx
@@ -1,1 +1,9 @@
-//TODO: add update
+const UpdateNote = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <h1 className="text-2xl font-semibold">Update Note</h1>
+    </div>
+  );
+};
+
+export default UpdateNote;
